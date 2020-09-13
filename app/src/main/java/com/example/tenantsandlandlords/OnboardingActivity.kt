fbf3bc60c29e409f6dded0bf32a89216d130d8d1
GeraudLuku <lukugeraud97@gmail.com>
2020-09-13 22:23:52 +0100

    added circular image view library

---
+++ app/src/main/java/com/example/tenantsandlandlords/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tenantsandlandlords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+    }
+}
