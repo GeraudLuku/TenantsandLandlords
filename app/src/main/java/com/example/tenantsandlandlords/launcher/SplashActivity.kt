7354664ae16425bdaaacd9d554f98a215a539928
GeraudLuku <lukugeraud97@gmail.com>
2020-09-13 21:57:51 +0100

    created a launcher package and added SplashActivity.kt under it

--- app/src/main/java/com/example/tenantsandlandlords/launcher/SplashActivity.kt
+++ app/src/main/java/com/example/tenantsandlandlords/launcher/SplashActivity.kt
@@ -1,15 +1,16 @@
-package com.example.tenantsandlandlords
+package com.example.tenantsandlandlords.launcher
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tenantsandlandlords.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //start intent to go to main activity
-        val intent = Intent(this,MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
